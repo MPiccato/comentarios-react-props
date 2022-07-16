55ae12265eb696e2565b282ec95ac8c7b10b220d
mpiccato <mpiccato@me.com>
2022-07-16 12:37:17 -0300

    agregado en negrita

--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -10,8 +10,9 @@ const Testimonio = (props) => {
             alt="testimonio" 
         />
         <div className='contenedor-texto-testimonio'>
-            <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
-            <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
+            <p 
+              className='nombre-testimonio'><strong>{props.nombre}</strong> en {props.pais}</p>
+            <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
             <p className='texto-testimonio'>{props.testimonio} </p>
         </div>
     </div>
